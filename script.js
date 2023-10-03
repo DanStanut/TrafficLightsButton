bee812d2cea39cfdb82307b100714a0716d8be57
DanStanut <dan.stanut@gmail.com>
2023-10-03 15:27:51 +0300

    Funtional

---
+++ script.js
@@ -0,0 +1,40 @@
+var state = 1;
+var counter = 0;
+setInterval(increaseTime, 1000)
+function increaseTime() {
+    ++counter
+    if (counter == 10) {
+        ++state
+        counter = 0
+        if (state > 3) {
+            state = 1
+        }
+        if (state == 1) {
+            document.getElementById("myButton").innerHTML = "Green"
+            document.getElementById("myButton").className = "btn btn-success"
+        } else if (state == 2) {
+            document.getElementById("myButton").innerHTML = "Yellow"
+            document.getElementById("myButton").className = "btn btn-warning"
+        } else {
+            document.getElementById("myButton").innerHTML = "Red"
+            document.getElementById("myButton").className = "btn btn-danger"
+        }
+    }
+}
+function changeState() {
+    ++state
+    counter = 0
+    if (state > 3) {
+        state = 1
+    }
+    if (state == 1) {
+        document.getElementById("myButton").innerHTML = "Green"
+        document.getElementById("myButton").className = "btn btn-success"
+    } else if (state == 2) {
+        document.getElementById("myButton").innerHTML = "Yellow"
+        document.getElementById("myButton").className = "btn btn-warning"
+    } else {
+        document.getElementById("myButton").innerHTML = "Red"
+        document.getElementById("myButton").className = "btn btn-danger"
+    }
+}
