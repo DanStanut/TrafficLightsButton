9b9356d8335ace50f48e7d4f39592a220af1433e
DanStanut <dan.stanut@gmail.com>
2023-10-03 15:33:24 +0300

    Simplified display function

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 var state = 1;
 var counter = 0;
 setInterval(increaseTime, 1000)
-function increaseTime() {
-    ++counter
-    if (counter == 10) {
-        ++state
-        counter = 0
-        if (state > 3) {
-            state = 1
-        }
-        if (state == 1) {
-            document.getElementById("myButton").innerHTML = "Green"
-            document.getElementById("myButton").className = "btn btn-success"
-        } else if (state == 2) {
-            document.getElementById("myButton").innerHTML = "Yellow"
-            document.getElementById("myButton").className = "btn btn-warning"
-        } else {
-            document.getElementById("myButton").innerHTML = "Red"
-            document.getElementById("myButton").className = "btn btn-danger"
-        }
-    }
-}
-function changeState() {
-    ++state
-    counter = 0
+function displayResult() {
     if (state > 3) {
         state = 1
     }
@@ -37,4 +15,17 @@ function changeState() {
         document.getElementById("myButton").innerHTML = "Red"
         document.getElementById("myButton").className = "btn btn-danger"
     }
+}
+function increaseTime() {
+    ++counter
+    if (counter == 10) {
+        ++state
+        counter = 0
+        displayResult()
+    }
+}
+function changeState() {
+    ++state
+    counter = 0
+    displayResult()
 }
