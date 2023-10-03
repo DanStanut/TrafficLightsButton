5bbce6a1a2302df5a9d9d85f0c4c3aa0f9dff20f
DanStanut <dan.stanut@gmail.com>
2023-10-03 15:58:32 +0300

    Counter added

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var state = 1;
-var counter = 0;
+var counter = 10;
 setInterval(increaseTime, 1000)
 function displayResult() {
     if (state > 3) {
         state = 1
     }
     if (state == 1) {
-        document.getElementById("myButton").innerHTML = "Green"
+        document.getElementById("myButton").innerHTML = "Green " + counter
         document.getElementById("myButton").className = "btn btn-success"
     } else if (state == 2) {
-        document.getElementById("myButton").innerHTML = "Yellow"
+        document.getElementById("myButton").innerHTML = "Yellow " + counter
         document.getElementById("myButton").className = "btn btn-warning"
     } else {
-        document.getElementById("myButton").innerHTML = "Red"
+        document.getElementById("myButton").innerHTML = "Red " + counter
         document.getElementById("myButton").className = "btn btn-danger"
     }
 }
 function increaseTime() {
-    ++counter
-    if (counter == 10) {
+    --counter
+    if (counter == 0) {
         ++state
-        counter = 0
-        displayResult()
+        counter = 10
     }
+    displayResult()
 }
 function changeState() {
     ++state
-    counter = 0
+    counter = 10
     displayResult()
 }
