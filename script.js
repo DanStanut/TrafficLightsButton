2caac3e28a42f2a0964c30fcb007bc1c136248e4
DanStanut <dan.stanut@gmail.com>
2023-10-04 14:32:20 +0300

    Feedback implemented

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let state = 1
 let counter = 10
-setInterval(increaseTime, 1000)
+setInterval(decreaseTime, 1000)
+
+function buttonDisplay(color, cnt, btnType) {
+    document.getElementById("myButton").innerHTML = color + cnt
+    document.getElementById("myButton").className = btnType
+}
+
 function displayResult() {
     if (state == 1) {
-        document.getElementById("myButton").innerHTML = "Green " + counter
-        document.getElementById("myButton").className = "btn btn-success"
+        buttonDisplay("Green ", counter, "btn btn-success")
     } else if (state == 2) {
-        document.getElementById("myButton").innerHTML = "Yellow " + counter
-        document.getElementById("myButton").className = "btn btn-warning"
+        buttonDisplay("Yellow ", counter, "btn btn-warning")
     } else {
-        document.getElementById("myButton").innerHTML = "Red " + counter
-        document.getElementById("myButton").className = "btn btn-danger"
+        buttonDisplay("Red ", counter, "btn btn-danger")
     }
 }
-function increaseTime() {
+
+function decreaseTime() {
     --counter
     if (counter == 0) {
         ++state
@@ -24,6 +28,7 @@ function increaseTime() {
     }
     displayResult()
 }
+
 function changeState() {
     ++state
     counter = 10
