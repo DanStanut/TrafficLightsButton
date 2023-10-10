f23bea86c1e301c9ba2b7308c6621f027028e583
Dan STANUT <60068096+DanStanut@users.noreply.github.com>
2023-10-10 10:48:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-let state = 1
+const buttonStates = ["btn btn-success", "btn btn-warning", "btn btn-danger"]
+let state = 0
+
 setInterval(changeState, 10000)
 
-function buttonDisplay(color, btnType) {
-    document.getElementById("myButton").innerHTML = color
+function buttonDisplay(btnType) {
     document.getElementById("myButton").className = btnType
 }
 
 function displayResult() {
-    switch (state) {
-        case 1 : 
-            buttonDisplay("Green ", "btn btn-success");
-			break;
-        case 2 : 
-            buttonDisplay("Yellow ", "btn btn-warning");
-			break;
-        default:
-            buttonDisplay("Red ", "btn btn-danger");
-    }
+    buttonDisplay(buttonStates[state]);
 }
 
 function changeState() {
     ++state
-    if (state > 3) {
-        state = 1
+    if (state > 2) {
+        state = 0
     }
     displayResult()
 }
